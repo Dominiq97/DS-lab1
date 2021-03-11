5b8db11d6af22a39c50b5fe34a8b2928efe33639
Dominic Catana <catanadominic@gmail.com>
2021-03-11 22:56:15 +0200

    materials added and imported

--- lab2/src/app/app.component.ts
+++ lab2/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculator';
+
+  ngOnInit() {
+    $('button').click(function() {alert('Alert!');});
+  }
+
 }
+declare var $:any;
--- lab2/src/app/app.module.ts
+++ lab2/src/app/app.module.ts
@@ -3,6 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CalculatorComponent } from './calculator/calculator.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import{ MatInputModule } from '@angular/material/input';
+import{ MatButtonModule } from '@angular/material/button';
+import{ MatCardModule } from '@angular/material/card';
+import{ MatFormFieldModule } from '@angular/material/form-field';
+import{ MatCheckboxModule } from '@angular/material/checkbox';
+import{ MatDatepickerModule } from '@angular/material/datepicker';
+import{ MatRadioModule } from '@angular/material/radio';
+import{ MatSelectModule} from '@angular/material/select';
+import{ BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +21,8 @@ import { CalculatorComponent } from './calculator/calculator.component';
     CalculatorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
